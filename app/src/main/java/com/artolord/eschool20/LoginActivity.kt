dcc4b9cbbff12d7e24a508bace3c852fb0f24232
ArtoLord <artgoi111@gmail.com>
2018-11-30 15:30:26 +0300

    Initial commit

---
+++ app/src/main/java/com/artolord/eschool20/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.artolord.eschool20
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
